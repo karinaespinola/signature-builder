1be53c1a72f4fa896cc4d7b47c8766922a2a9c5a
Karina Espinola <k.espinolah@gmail.com>
2021-01-12 17:23:07 -0600

    Use context to select default menu tab

--- src/components/builder/MenuTabs.js
+++ src/components/builder/MenuTabs.js
@@ -2,20 +2,29 @@ import React from 'react';
 import {Tabs,Tab, Col} from 'react-bootstrap';
 import General from './General';
 import Templates from './Templates/Templates';
+import {UserDataContext} from '../../contexts/UserDataProvider';
 
 const MenuTabs = () => {
+    const {userData, updateUserData} = React.useContext(UserDataContext);
+
     return (
         <>
-            <Tabs defaultActiveKey="general" id="uncontrolled-tab-example">
+            <Tabs
+            defaultActiveKey={userData.menuTab} 
+            activeKey={userData.menuTab} 
+            id="menu-tabs"
+            onSelect={(key) => updateUserData({...userData, menuTab: key})}
+            >
+                <Tab eventKey="templates" title="Templates">
+                    <Templates />
+                </Tab>
                 <Tab eventKey="general" title="General">
                     <General />
                 </Tab>
                 <Tab eventKey="images" title="Images">
                     2
                 </Tab>
-                <Tab eventKey="templates" title="Templates">
-                    <Templates />
-                </Tab>
+
             </Tabs>
         </>
     )
