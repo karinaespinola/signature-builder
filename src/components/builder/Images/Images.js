48b0e256c068c34a29e6532149064eaa955eccfb
Karina Espinola <k.espinolah@gmail.com>
2021-01-18 15:36:39 -0600

    Changes to use the ImagePreview component

--- src/components/builder/Images/Images.js
+++ src/components/builder/Images/Images.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {UserDataContext} from '../../../contexts/UserDataProvider';
 import UploadImage from './UploadImage';
+import AddImageLink from './AddImageLink';
 
 const Images = () => {
+  const {userData, updateUserData} = React.useContext(UserDataContext);
+  const [avatarImageUrl, setAvatarImageUrl] = React.useState(null);
+
+  const updateAvatarImageUrl = (url) => {
+    setAvatarImageUrl(url);
+    updateUserData({...userData, avatarImageUrl: url});
+  }
+
     return (
         <>
-          <UploadImage />  
+          <UploadImage 
+          updateContextProperty={updateAvatarImageUrl} 
+          imageWidth={300} 
+          imageHeight={300} 
+          imagePreviewUrl={userData.avatarImageUrl}
+          previewWidth={150}
+          showDeleteButton={true}
+          />  
         </>
     )
 }
