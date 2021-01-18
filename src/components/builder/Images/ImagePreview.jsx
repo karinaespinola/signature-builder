3861f85c0cb1ed8dce7ed1c4d4750d4ac5cba861
Karina Espinola <k.espinolah@gmail.com>
2021-01-18 15:35:42 -0600

    Add styles to the ImagrePreview component

--- src/components/builder/Images/ImagePreview.jsx
+++ src/components/builder/Images/ImagePreview.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
-const ImagePreview = () => {
+const ImagePreview = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const {src, showDeleteButton, previewWidth} = props;
     return (
-        <div>
-            
+        <div style={{position: 'relative', maxWidth: previewWidth}}>
+            <img 
+            src={src} 
+            alt="" 
+            style={{maxWidth: '100%'}}
+            />
+            {
+                showDeleteButton
+                ? <>
+                    <span className="delete-image-button">
+                        <FontAwesomeIcon icon={faTimes} role="button"/>
+                    </span>                    
+                 </>
+                : null
+            }
+
         </div>
     )
 }
