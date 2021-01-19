24a0def36d879c6e47bb6324d2d34cb9e82a39a1
Karina Espinola <k.espinolah@gmail.com>
2021-01-19 17:41:14 -0600

    Get banners from the database

--- src/components/builder/Images/BannerDropDown.jsx
+++ src/components/builder/Images/BannerDropDown.jsx
@@ -1,45 +1,42 @@
 import React from 'react';
 import {DropdownButton, Dropdown} from 'react-bootstrap';
-import Banner1 from '../../../img/banner-1.png';
-import Banner2 from '../../../img/banner-2.png';
-import {storage} from '../../../firebase/firebase';
+import 'firebase/database';
+import {storage, database} from '../../../firebase/firebase';
+
 
 const BannerDropDown = () => {
     const [banners, setBanners] = React.useState([]);
 
     React.useEffect(() => {
-        let newArray = [];
-
-        storage
-        .ref("banners")
-        .listAll()
-        .then(function(res) {
-            res.items.forEach(function(itemRef) {
-                itemRef.getDownloadURL()
-                .then((url) => {
-                    console.log(url)
-                    newArray.push(url);
-                })
-                .catch((error) => console.log(error))            
-            });
-            setBanners(newArray);
-          }).catch(function(error) {
-            console.log(error);
-          });
-          
+        getBanners();    
     }, [])
+
+    const getBanners = () => {
+        let _records = [];
+        database.collection("banners").get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                _records.push({id: doc.id, ...doc.data()});
+            });
+            setBanners(_records);
+        });       
+    }
+
     return (
         <div>
             <DropdownButton id="dropdown-item-button" title="Pick a banner" variant="primary" className="banner-dropdown" size="lg">
                 <Dropdown.Item as="button" eventKey="none">
                     None
                 </Dropdown.Item>
-                <Dropdown.Item as="button" eventKey="1">
-                    <img src={Banner1}></img>
-                </Dropdown.Item>
-                <Dropdown.Item as="button" eventKey="2">
-                    <img src={Banner2}></img>
-                </Dropdown.Item>
+                {
+                    banners.map((banner) => 
+                        (
+                            <Dropdown.Item as="button" key={banner.id} eventKey={banner.id}>
+                                <img src={banner.url}></img>
+                            </Dropdown.Item>
+                        )
+                    )
+                }
             </DropdownButton>
         </div>
     )
